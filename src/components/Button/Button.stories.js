eab58dd2214ebe2d542f01077d278564239116f5
Norman Calhoun <ndc1963@gmail.com>
2020-02-21 16:12:04 -0500

    first commit

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import "./Button.css";
+import Button from "./Button";
 
-import Button from './Button';
-
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+const style = {
+  backgroundColor: "#0069D9",
+  color: "white",
+  borderRadius: "2px",
+  padding: "5px 25px",
+  border: "#0069D9"
+};
+const dangerStyle = {
+  backgroundColor: "red"
+};
+const successStyle = {
+  backgroundColor: "green"
+};
+const warningStyle = {
+  backgroundColor: "yellow"
+};
+//This is adding attributes that we can access later.
+storiesOf("Button", module)
+  .add("Primary", () => <Button label="Primary" type="primary" style={style} />)
+  .add("Large Primary", () => (
+    <Button label="Large Primary Button" type="primary" large />
+  ))
+  .add("Danger", () => (
+    <Button label="Danger" type="danger" style={dangerStyle} />
+  ))
+  .add("Success", () => (
+    <Button label="Success" type="success" style={successStyle} />
+  ))
+  .add("Warning", () => (
+    <Button label="Warning" type="warning" style={warningStyle} />
+  ));
