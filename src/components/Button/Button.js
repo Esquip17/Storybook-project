b9330769e2cf31cd1df66ba4a634ecc783b077ce
Norman Calhoun <ndc1963@gmail.com>
2020-02-21 17:00:20 -0500

    button fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = props => <button style={props.style}>{props.label}</button>;
-let classList = "";
-let types = ["primary", "danger", "success", "warning"];
-if (types.includes(props.types)) {
-  classList += `button-${props.type}`;
-}
-if (props.large) {
-  classList += ` button-large`; // Note the spacing here since we are adding to the string!
-}
+const Button = props => {
+  {
+    /* <button style={props.style}>{props.label}</button> */
+  }
+  let classList = "";
+  let types = ["primary", "danger", "success", "warning"];
+  if (types.includes(props.types)) {
+    classList += `button-${props.type}`;
+  }
+  if (props.large) {
+    classList += ` button-large`; // Note the spacing here since we are adding to the string!
+  }
 
-//  Give the button's class a value of classList
-return <button className={classList}>{props.label}</button>;
-
+  //  Give the button's class a value of classList
+  return <button className={classList}>{props.label}</button>;
+};
 export default Button;
